1d50c5722a686a933c8b935f1fb35767ace4faf2
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2024-11-28 14:26:36 +0900

    feat: :heavy_plus_sign: Add bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+
+const GAMETITLE: &str = "Bevyセットアップコード";
+const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
+const BACKGROUND_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: WINDOW_SIZE.into(),
+                    title: GAMETITLE.to_string(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+        )
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .insert_resource(Time::<Fixed>::from_seconds(1.0 / 60.0))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+) {
+    println!("main: setup camera");
+    commands.spawn(Camera2dBundle::default());
 }
