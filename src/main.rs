505cd161a53a893e893a2e5ce87c04d23bbb8741
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2024-11-28 14:28:13 +0900

    feat: :loud_sound: Add bgm

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 const GAMETITLE: &str = "Bevyセットアップコード";
 const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
 const BACKGROUND_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
+const PATH_SOUND_BGM: &str = "sounds/bgm.ogg";
 
 fn main() {
     App::new()
@@ -24,7 +25,18 @@ fn main() {
 
 fn setup(
     mut commands: Commands,
+    asset_server: Res<AssetServer>,
 ) {
     println!("main: setup camera");
     commands.spawn(Camera2dBundle::default());
+    println!("main: setup bgm");
+    let bgm_sound = asset_server.load(PATH_SOUND_BGM);
+
+    commands.spawn(
+        AudioBundle {
+            source: bgm_sound,
+            settings: PlaybackSettings::LOOP.with_spatial(true),
+        }
+    )
+    .insert(Name::new("bgm"));
 }
